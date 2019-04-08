843bca5680bd90b0a858d53c42c567c995ad21b1
Michael <43596340+KolyanPie@users.noreply.github.com>
2019-04-08 20:13:49 +0300

    add AlgorithmController

---
+++ src/main/java/main/controllers/AlgorithmController.java
@@ -0,0 +1,111 @@
+package main.controllers;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class AlgorithmController {
+    private ArrayList<String> rotates;
+    public static final List<String> cube2x2x2 = Arrays.asList("U", "R", "F", "U'", "R'", "F'", "D", "L", "B", "D'", "L'", "B'", "U2", "R2", "F2", "D2", "L2", "B2");
+
+    public AlgorithmController(List<String> rotates) {
+        this.rotates = new ArrayList<>(rotates);
+    }
+
+    public String simplify(String algorithm) throws IllegalArgumentException {
+        if (algorithm.equals("")) {
+            return "";
+        }
+        String[] strings = algorithm.split(" ");
+        return simplify(strings);
+    }
+
+    public String simplify(String... algorithm) throws IllegalArgumentException {
+        ArrayList<String> strings = new ArrayList<>(Arrays.asList(algorithm));
+        ArrayList<String> strings1 = new ArrayList<>(Arrays.asList(algorithm));
+        simplifyPare(strings1);
+        while (!strings.equals(strings1)) {
+            strings = new ArrayList<>(strings1);
+            simplifyPare(strings1);
+        }
+        String result = strings.toString();
+        result = result.replaceAll(",", "");
+        return result.substring(1, result.length() - 1);
+    }
+
+    private void simplifyPare(ArrayList<String> algorithm) throws IllegalArgumentException {
+        if (algorithm.size() == 0) {
+            return;
+        }
+        String s = algorithm.get(0);
+        if (!rotates.contains(s)) {
+            throw new IllegalArgumentException(s);
+        }
+        for (int i = 1; i < algorithm.size(); i++) {
+            s = algorithm.get(i - 1);
+            if (!rotates.contains(algorithm.get(i))) {
+                throw new IllegalArgumentException(algorithm.get(i));
+            }
+            if (algorithm.get(i).substring(0, 1).equals(s.substring(0, 1))) {
+                if (algorithm.get(i).length() == 1) {
+                    if (s.length() == 1) {
+                        algorithm.remove(i);
+                        algorithm.remove(i - 1);
+                        algorithm.add(i - 1, s + "2");
+                        return;
+                    }
+                    switch (s.charAt(1)) {
+                        case '\'':
+                            algorithm.remove(i);
+                            algorithm.remove(i - 1);
+                            return;
+                        case '2':
+                            algorithm.remove(i);
+                            algorithm.remove(i - 1);
+                            algorithm.add(i - 1, s.substring(0, 1) + "'");
+                            return;
+                    }
+                }
+                if (algorithm.get(i).length() == 2) {
+                    switch (algorithm.get(i).charAt(1)) {
+                        case '\'':
+                            if (s.length() == 1) {
+                                algorithm.remove(i);
+                                algorithm.remove(i - 1);
+                                return;
+                            }
+                            switch (s.charAt(1)) {
+                                case '\'':
+                                    algorithm.remove(i);
+                                    algorithm.remove(i - 1);
+                                    algorithm.add(i - 1, s.substring(0, 1) + "2");
+                                    return;
+                                case '2':
+                                    algorithm.remove(i);
+                                    algorithm.remove(i - 1);
+                                    algorithm.add(i - 1, s.substring(0, 1));
+                                    return;
+                            }
+                        case '2':
+                            if (s.length() == 1) {
+                                algorithm.remove(i);
+                                algorithm.remove(i - 1);
+                                algorithm.add(i - 1, s.substring(0, 1) + "'");
+                                return;
+                            }
+                            switch (s.charAt(1)) {
+                                case '\'':
+                                    algorithm.remove(i);
+                                    algorithm.remove(i - 1);
+                                    algorithm.add(i - 1, s.substring(0, 1));
+                                    return;
+                                case '2':
+                                    algorithm.remove(i);
+                                    algorithm.remove(i - 1);
+                                    return;
+                            }
+                    }
+                }
+            }
+        }
+    }
+}
--- src/main/java/visual/controllers/MainController.java
+++ src/main/java/visual/controllers/MainController.java
@@ -1,19 +1,25 @@
 package visual.controllers;
 
+import com.sun.javafx.tk.Toolkit;
+import javafx.concurrent.Service;
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import main.Cube;
+import main.controllers.AlgorithmController;
 import main.controllers.CubeController;
 import visual.nodes.UnfoldedCubeCanvas;
 
 public class MainController {
     private Cube cube;
     private CubeController cubeController;
+    private AlgorithmController algorithmController2x2;
     @FXML
     private UnfoldedCubeCanvas cubeCanvas;
     @FXML
@@ -22,11 +28,14 @@ public class MainController {
     private Label label;
     @FXML
     private Button resetButton;
+    @FXML
+    public MenuItem simplifyLabel;
 
     @FXML
     public void initialize() {
         cube = new Cube();
         cubeController = new CubeController();
+        algorithmController2x2 = new AlgorithmController(AlgorithmController.cube2x2x2);
         cubeCanvas.getGraphicsContext2D().setFill(Color.DARKGRAY);
         cubeCanvas.getGraphicsContext2D().fillRect(0, 0, cubeCanvas.getWidth(), cubeCanvas.getHeight());
         cubeCanvas.drawCube(cube);
@@ -34,7 +43,11 @@ public class MainController {
             button.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
                 cubeController.rotate(cube, ((Button) button).getText());
                 cubeCanvas.drawCube(cube);
-                label.setText(label.getText() + " " + ((Button) button).getText());
+                if (label.getText().equals("")) {
+                    label.setText(((Button) button).getText());
+                } else {
+                    label.setText(label.getText() + " " + ((Button) button).getText());
+                }
             });
         }
         resetButton.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
@@ -42,5 +55,12 @@ public class MainController {
             cube = new Cube();
             cubeCanvas.drawCube(cube);
         });
+        simplifyLabel.setOnAction(event -> {
+            String oldAlg = label.getText();
+            System.out.println("alg: (" + oldAlg + ")");
+            String newAlg = algorithmController2x2.simplify(oldAlg);
+            label.setText(newAlg);
+        });
+
     }
 }
